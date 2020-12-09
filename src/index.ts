369e102d1be7addda7230f622f5528fbbf520f40
Joe Flateau <joe@joeflateau.net>
2020-12-08 22:31:47 -0500

    add sync variant

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import { spawnSync } from "child_process";
 import { findPriority } from "./findPriority";
 
 async function run(listenerArn: string, hostname: string) {
@@ -10,4 +11,10 @@ async function run(listenerArn: string, hostname: string) {
 if (require.main === module) {
   const [, , listenerArn, hostname] = process.argv;
   run(listenerArn, hostname);
+}
+
+export function getPrioritySync(listenerArn: string, hostname: string) {
+  return Number(
+    spawnSync(`npx`, ["elb-rule-priority", listenerArn, hostname]).stdout
+  );
 }
